eb28c14f368019bf9e1ced67965d9117d42c2b73
Cyberod <110993874+Cyberod@users.noreply.github.com>
2025-02-06 12:38:20 +0100

    Updated main file to use the calculations function

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 from plan import choose_budget_plan, get_custom_plan
+from utils import get_valid_number
+from calculations import calculate_category_amounts
 
 def main():
     print("💰 Welcome to the Budget Automation System! 💰")
+
+    # choose the total amount to budget
+    total_amount = get_valid_number("Enter the total amount to budget: ")
 
     choice = choose_budget_plan()
 
     if choice == 1:
         budget_plan = {"Expenses": 50, "Investment": 30, "Savings": 20}
+        plan_name = "Conservative (50-30-20)"
     elif choice == 2:
         budget_plan = {"Expenses": 60, "Investment": 20, "Savings": 20}
+        plan_name = "Moderate (60-20-20)"
     elif choice == 3:
         budget_plan = {"Expenses": 70, "Investment": 10, "Savings": 20}
+        plan_name = "Aggressive (70-10-20)"
     elif choice == 4:
-        custom_plan_name, custom_plan = get_custom_plan()
+        plan_name, budget_plan = get_custom_plan()
+
+
+    # Calculate amounts for each category
     
-        print(f"\n📊 Your Selected Budget Plan: {custom_plan_name}")
-        for category, percentage in custom_plan.items():
-            print(f"   -{category}: {percentage}%")
+    category_amounts = calculate_category_amounts(total_amount, budget_plan)
+
+    # Display the budget plan
+    print(f"\n📊 Your Budget Breakdown for {plan_name}")
+    print(f"Total Amount: ${total_amount:.2f}")
+    print(f" Category Breakdown")
+
+    for category, amount in category_amounts.items():
+        percentage = budget_plan[category]
+        print(f"    - {category}: ({percentage}%) : ${amount:.2f} ")
 
 
 if __name__ == "__main__":
